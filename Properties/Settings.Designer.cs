43c8d5f40278cc4e19a984dcdc5fd72e33d7cd79
Administrator <alex_m_b@mail.ru>
2024-12-20 17:40:50 +0300

    Improving the default settings of the program.

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -109,7 +109,7 @@ namespace SimpleSmtpMailSender.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("xxx")]
+        [global::System.Configuration.DefaultSettingValueAttribute("xxxxxxxx")]
         public string Password {
             get {
                 return ((string)(this["Password"]));
